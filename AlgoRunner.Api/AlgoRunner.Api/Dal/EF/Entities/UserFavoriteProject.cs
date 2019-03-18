71dbedba8dcaa27364d65288cd317c9c2b29d0ca
Amir capuano <capuano@gmail.com>
2019-03-18 13:47:44 +0200

    Add user favorite project EF/DB entity

---
+++ AlgoRunner.Api/AlgoRunner.Api/Dal/EF/Entities/UserFavoriteProject.cs
@@ -0,0 +1,27 @@
+﻿namespace AlgoRunner.Api.Dal.EF.Entities
+{
+    using AutoMapper;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    public partial class UserFavoriteProject
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [IgnoreMap]
+        public int Id { get; set; }
+        public User User { get; set; }
+        public int ProjectId { get; set; }
+        [ForeignKey("ProjectId")]
+        public Project Project { get; set; }
+
+        [NotMapped]
+        public int UserId
+        {
+            get
+            {
+                return User != null ? User.Id : int.MinValue;
+            }
+        }
+    }
+}
